711ce5aef75f56f94e8ac42fdf9934c74c426053
NandoDG <nando@puzzlefilmes.com.br>
2020-04-25 20:57:38 -0300

    criado controller e rotas

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,7 +6,40 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index(){
-        return 'listagem dos Produtos';
+    public function index()
+    {
+      
+        $products = ['Product1', 'Product2', 'Product3'];
+
+        return $products;
+    }
+    public function show($id)
+    {
+        return "exibindo o produto de id: ($id)";
+    }
+
+    public function create()
+    {
+        return "exibindo o formulario de cadastro de um novo produto";
+    }
+
+    public function edit($id)
+    {
+        return "formulario para editar o produto: {$id}";
+    }
+
+    public function store()
+    {
+        return "Cadastro de um novo produto";
+    }
+
+    public function update($id)
+    {
+        return "Editando o produto: {$id}";
+    }
+
+    public function destroy($id)
+    {
+        return "Deletando o produto: {$id}";
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,22 @@
 <?php
 
+Route::resource('products', 'ProductControler');
+
+/*
+Route::delete('products/{id}', 'ProductController@destroy')->name('products.destroy');
+Route::put('products/{id}', 'ProductController@update')->name('products.update');
+Route::get('products/{id}/edit', 'ProductController@edit')->name('products.edit');
+Route::get('products/create', 'ProductController@create')->name('products.create');
+Route::get('products/{id}', 'ProductController@show')->name('products.show');
+Route::get('products', 'ProductController@index')->name('products.index');
+Route::post('products', 'ProductController@store')->name('products.store');
+*/
+Route::get('/login', function (){
+    return ' login';
+})->name('login');
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('/contato' , function(){
-    return view('contato');
-});
-
-Route::get('/blog' , function(){
-    return 'BLOG';
-});
-
 
 
