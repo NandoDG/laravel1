750660b3bcb29c0cf31ef50f44c4fa59ebb8bbaa
NandoDG <nando@puzzlefilmes.com.br>
2020-04-25 08:19:13 -0300

    trabalhando no entendimento das rotas do laravel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('/2', function () {
+    return view('contato');
+});
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
